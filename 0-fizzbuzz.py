67c3a4f569978ff3daae96065c791fd1f08c4b23
EllyIan <ronixeli45@gmail.com>
2024-02-26 19:29:25 +0300

    fizzbuzz debug

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -29,16 +29,11 @@ def fizzbuzz(n):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Invalid number of arguments")
+    if len(sys.argv) <= 1:
+        print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    try:
-        number = int(sys.argv[1])
-    except ValueError:
-        print("Invalid number format. Please provide an integer.")
-        sys.exit(1)
-
+    number = int(sys.argv[1])
     fizzbuzz(number)
